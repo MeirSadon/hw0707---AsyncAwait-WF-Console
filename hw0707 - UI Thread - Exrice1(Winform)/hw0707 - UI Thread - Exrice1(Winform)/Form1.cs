81bb7648e947900c5d01444e61967f8918fb1ddc
מאיר סעדון <45739995+MeirSadon@users.noreply.github.com>
2019-07-10 18:13:19 +0300

    Add files via upload

---
+++ hw0707 - UI Thread - Exrice1(Winform)/hw0707 - UI Thread - Exrice1(Winform)/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hw0707___UI_Thread___Exrice1_Winform_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                await Task.Run(() =>
+                {
+                    Thread.Sleep(1000);
+                });
+                label1.Text = DateTime.Now.Millisecond.ToString();
+                label1.ForeColor = DateTime.Now.Second % 2 == 0 ? Color.Green : Color.Red;
+            }
+        }
+    }
+}
---
+++ hw0707 - UI Thread - Exrice1(Winform)/hw0707 - UI Thread - Exrice1(Winform)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hw0707___UI_Thread___Exrice1_Winform_
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
