adc6b3b3b2da43e419f9361431ab32af34d1229d
מאיר סעדון <45739995+MeirSadon@users.noreply.github.com>
2019-07-10 18:42:19 +0300

    Add files via upload

---
+++ hw0707 - WriteToFile - Exrice2(Console)/hw0707 - WriteToFile - Exrice2(Console)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace hw0707___WriteToFile___Exrice2_Console_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t = WriteTill1000();
+            while(!t.Wait(100))
+            {
+                Console.Write(".");
+            };
+            Console.WriteLine("Done...........................");
+        }
+
+        public static async Task WriteTill1000()
+        {
+            StreamWriter reader = new StreamWriter(@"C:\temp\NumberFile.txt", true);
+            for (int i = 0; i <= 1000; i++)
+                {
+                    await reader.WriteAsync(i.ToString() + " ");
+                    Thread.Sleep(5);
+                }
+
+        }
+    }
+}
+
